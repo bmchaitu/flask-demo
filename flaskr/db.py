a6bd5006850197f8c80dd7f924e29299f1c2387e
Chaitanya <budagavichaitu@gmail.com>
2021-05-20 16:40:47 +0530

    Make changes

--- flaskr/db.py
+++ flaskr/db.py
@@ -18,13 +18,11 @@ def init_db():
     with current_app.open_resource('schema.sql') as f:
         db.executescript(f.read().decode('utf-8'))
     
-
+@with_appcontext
 init_db()
-
 
 def init_app(app):
     app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
 
 
 def close_db(e=None):
